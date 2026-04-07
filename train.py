5b69fa746465a1dd4ca7e82e224a8da09772ea56
ghousesatech-pixel <ghouse.satech@gmail.com>
2026-04-07 13:58:24 -0400

    Update train.py

--- train.py
+++ train.py
@@ -9,3 +9,10 @@ train_df, test_df = train_test_split(df, test_size=0.2, random_state=42)
 
 dataiku.Dataset("train_data").write_with_schema(train_df)
 dataiku.Dataset("test_data").write_with_schema(test_df)
+
+
+
+mkdir -p mirror/registry.terraform.io/ratcliffm/pyscript/1.0.0/linux_amd64
+cp bin/terraform-provider-pyscript_v1.0.0 mirror/registry.terraform.io/ratcliffm/pyscript/1.0.0/linux_amd64/
+chmod +x mirror/registry.terraform.io/ratcliffm/pyscript/1.0.0/linux_amd64/terraform-provider-pyscript_v1.0.0
+find mirror -type f
